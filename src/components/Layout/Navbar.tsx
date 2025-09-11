90b09256dac3602bfff7fff05adfe8902c387e98
devnawa <148815756+devnawa@users.noreply.github.com>
2025-09-11 15:07:57 +0300

    fix: add navbar home router

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image'
 import React from 'react'
 import DiscordButton from './DiscordButton'
+import Link from 'next/link'
 
 const Navbar = () => {
 
     return (
         <div className="bg-solbabe-purple backdrop-blur-sm w-screen min-w-screen h-[4rem] py-2 px-8 flex items-center justify-between">
-            <div className="flex items-center gap-2 h-[4rem]">
+            <Link href="/" className="flex items-center gap-2 h-[4rem]">
                 <Image src="/photos/sblogo.jpg" alt="SolBabes" width={400} height={400} className="h-full w-fit" />
                 <p className="text-white md:text-2xl text-lg font-bold italic">Memory Game</p>
-            </div>
+            </Link>
             <DiscordButton />
         </div>
     )
