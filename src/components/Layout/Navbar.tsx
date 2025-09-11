77005cb83f74f4ac8cd31fda4937c891bf2cbda8
devnawa <148815756+devnawa@users.noreply.github.com>
2025-09-11 16:08:39 +0300

    fix: navbar logo url

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     return (
         <div className="bg-solbabe-purple backdrop-blur-sm w-screen min-w-screen h-[4rem] py-2 px-8 flex items-center justify-between">
             <Link href="/" className="flex items-center gap-2 h-[4rem]">
-                <Image src="/photos/sblogo.jpg" alt="SolBabes" width={400} height={400} className="h-full w-fit" />
+                <Image src="/logo.png" alt="SolBabes" width={400} height={400} className="h-full w-fit" />
                 <p className="text-white md:text-2xl text-lg font-bold italic">Memory Game</p>
             </Link>
             <DiscordButton />
