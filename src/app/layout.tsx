a5d42071bc462620afe5d4032978e63fa116c1af
devnawa <148815756+devnawa@users.noreply.github.com>
2025-09-11 19:05:28 +0300

    feat: preload images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,42 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" as="image" href="/game/0.png" />
+        <link rel="preload" as="image" href="/game/1.png" />
+        <link rel="preload" as="image" href="/game/2.png" />
+        <link rel="preload" as="image" href="/game/3.png" />
+        <link rel="preload" as="image" href="/game/4.png" />
+        <link rel="preload" as="image" href="/game/5.png" />
+        <link rel="preload" as="image" href="/game/6.png" />
+        <link rel="preload" as="image" href="/game/7.png" />
+        <link rel="preload" as="image" href="/game/8.png" />
+        <link rel="preload" as="image" href="/game/9.png" />
+        <link rel="preload" as="image" href="/game/10.png" />
+        <link rel="preload" as="image" href="/game/11.png" />
+        <link rel="preload" as="image" href="/game/12.png" />
+        <link rel="preload" as="image" href="/game/13.png" />
+        <link rel="preload" as="image" href="/game/14.png" />
+        <link rel="preload" as="image" href="/game/15.png" />
+        <link rel="preload" as="image" href="/game/16.png" />
+        <link rel="preload" as="image" href="/game/17.png" />
+        <link rel="preload" as="image" href="/game/18.png" />
+        <link rel="preload" as="image" href="/game/19.png" />
+        <link rel="preload" as="image" href="/game/20.png" />
+        <link rel="preload" as="image" href="/game/21.png" />
+        <link rel="preload" as="image" href="/game/22.png" />
+        <link rel="preload" as="image" href="/game/23.png" />
+        <link rel="preload" as="image" href="/game/24.png" />
+        <link rel="preload" as="image" href="/game/25.png" />
+        <link rel="preload" as="image" href="/game/26.png" />
+        <link rel="preload" as="image" href="/game/27.png" />
+        <link rel="preload" as="image" href="/game/28.png" />
+        <link rel="preload" as="image" href="/game/29.png" />
+        <link rel="preload" as="image" href="/game/30.png" />
+        <link rel="preload" as="image" href="/game/31.png" />
+        <link rel="preload" as="image" href="/game/32.png" />
+        <link rel="preload" as="image" href="/game/unfound.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
