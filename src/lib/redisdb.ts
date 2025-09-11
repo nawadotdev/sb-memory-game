10da04b95948d02e965a1a2a633bc39a76024489
devnawa <148815756+devnawa@users.noreply.github.com>
2025-09-11 17:10:34 +0300

    fix: redis reconnect

--- src/lib/redisdb.ts
+++ src/lib/redisdb.ts
@@ -1,27 +1,33 @@
-import { createClient } from 'redis';
+import { createClient } from "redis";
 
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 const REDIS_HOST = process.env.REDIS_HOST;
 
-
 const redisClient = createClient({
-    username: 'default',
-    password: REDIS_PASSWORD,
-    socket: {
-        host: REDIS_HOST,
-        port: 14679
-    }
+  username: "default",
+  password: REDIS_PASSWORD,
+  socket: {
+    host: REDIS_HOST,
+    port: 14679,
+    reconnectStrategy: (retries) => {
+      const delay = Math.min(retries * 1000, 30000);
+      return delay;
+    },
+  },
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis Client Error", err);
 });
 
 export const getRedisClient = async () => {
+  if (!REDIS_PASSWORD || !REDIS_HOST) {
+    throw new Error("REDIS_PASSWORD or REDIS_HOST is not set");
+  }
 
-    if (!REDIS_PASSWORD || !REDIS_HOST) {
-        throw new Error('REDIS_PASSWORD or REDIS_HOST is not set');
-    }
+  if (!redisClient.isOpen) {
+    await redisClient.connect();
+  }
 
-    if (!redisClient.isOpen) {
-        await redisClient.connect();
-    }
-    return redisClient;
-}
-
+  return redisClient;
+};
