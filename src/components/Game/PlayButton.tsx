097f6dd64f40af53fa3161e13272c5cecd3103fd
devnawa <148815756+devnawa@users.noreply.github.com>
2025-09-15 14:34:20 +0300

    style: play button states

--- src/components/Game/PlayButton.tsx
+++ src/components/Game/PlayButton.tsx
@@ -2,20 +2,25 @@
 
 import React, { useState } from 'react'
 import { Button } from '../ui/button'
-import { Loader2 } from 'lucide-react'
+import { Loader2, PlayIcon } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { toast } from 'react-hot-toast'
+import { useAuth } from '@/context/AuthContext'
 
 const PlayButton = () => {
 
     const [creatingGame, setCreatingGame] = useState(false)
     const router = useRouter()
+    const { user, isAuthenticating } = useAuth()
 
     const handlePlay = async () => {
         setCreatingGame(true)
-        try{
-            const response = await fetch("/api/game/play")
-            if(response.ok){
+        try {
+            const response = await fetch("/api/game/play", {
+                method: "GET",
+                credentials: "include",
+            })
+            if (response.ok) {
                 const game = await response.json()
                 console.log(game)
                 router.push(`/game/${game._id}`)
@@ -28,12 +33,17 @@ const PlayButton = () => {
     }
 
     return (
-        <Button variant="outline" className='cursor-pointer' onClick={handlePlay} disabled={creatingGame}>
-            {creatingGame ? (
-                <p className='flex items-center gap-2'><Loader2 className='animate-spin' /> Creating game...</p>
-            ) : (
-                <p className='flex items-center gap-2'>Play</p>
-            )}
+        <Button variant="outline" className='cursor-pointer' onClick={handlePlay} disabled={creatingGame || isAuthenticating || !user}>
+            <PlayIcon className='size-4' />
+            {isAuthenticating ? <Loader2 className='animate-spin' /> :
+                !user ? <p className='flex items-center gap-2'>Login to play</p> :
+                    (
+                        creatingGame ? (
+                            <p className='flex items-center gap-2'><Loader2 className='animate-spin' /> Creating game...</p>
+                        ) : (
+                            <p className='flex items-center gap-2'>Play</p>
+                        ))
+            }
         </Button>
     )
 }
